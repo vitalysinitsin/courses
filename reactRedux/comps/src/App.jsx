3f4c4d4f19abaeb9f07f9a2ff9b915f575e4fc9e
hungryvito <vsinitsin94@gmail.com>
2024-10-24 23:53:17 -0600

    setup navigation with routes

--- reactRedux/comps/src/App.jsx
+++ reactRedux/comps/src/App.jsx
@@ -1,10 +1,21 @@
 import Link from "./components/Link";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import Route from "./components/Route";
 
 function App() {
   return (
     <div>
       <Link to="/accordion">accordion</Link>
       <Link to="/dropdown">dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage></AccordionPage>
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage></DropdownPage>
+        </Route>
+      </div>
     </div>
   );
 }
