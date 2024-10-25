4f7892ee5c1df95123b39d7f05e25843760ba057
hungryvito <vsinitsin94@gmail.com>
2024-10-25 00:23:11 -0600

    update active path link styling, wrap up navigation

--- reactRedux/comps/src/components/Link.jsx
+++ reactRedux/comps/src/components/Link.jsx
@@ -1,9 +1,13 @@
 import classNames from "classnames";
 import useNavigation from "../hooks/use-navigation";
 
-function Link({ to, children }) {
-  const { navigate } = useNavigation();
-  const classes = classNames("text-blue-500 m-1");
+function Link({ to, children, className, activeClassName }) {
+  const { currentPath, navigate } = useNavigation();
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
