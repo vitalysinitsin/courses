0ae3c1cc918336fe5dc57e69ead8484c16b80197
hungryvito <vsinitsin94@gmail.com>
2024-10-25 00:00:22 -0600

    wrap the navigation with a hook and style the links

--- reactRedux/comps/src/components/Link.jsx
+++ reactRedux/comps/src/components/Link.jsx
@@ -1,15 +1,24 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+  const classes = classNames("text-blue-500 underline m-1");
 
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
