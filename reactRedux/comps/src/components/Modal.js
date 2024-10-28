02524de83cf62b02f7a457c6f90bef27da26d426
hungryvito <vsinitsin94@gmail.com>
2024-10-27 21:17:48 -0600

    adds modal interactivity (open/close)

--- reactRedux/comps/src/components/Modal.js
+++ reactRedux/comps/src/components/Modal.js
@@ -1,5 +1,16 @@
-function Modal() {
-  return <div>modal</div>;
+import ReactDOM from "react-dom";
+
+function Modal({ onClose, children }) {
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="absolute inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white">Modal</div>
+    </div>,
+    document.querySelector(".modal-container")
+  );
 }
 
 export default Modal;
--- reactRedux/comps/src/pages/ModalPage.jsx
+++ reactRedux/comps/src/pages/ModalPage.jsx
@@ -1,9 +1,24 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
+  const [showModal, setShowModal] = useState();
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div>
-      <Modal />
+    <div className="relative">
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && <Modal onClose={handleClose} />}
     </div>
   );
 }
