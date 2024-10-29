05ac1931941aac07c1a49deac5151c31c8765d6f
hungryvito <vsinitsin94@gmail.com>
2024-10-29 10:46:32 -0600

    setup a simple count form

--- reactRedux/comps/src/pages/CounterPage.jsx
+++ reactRedux/comps/src/pages/CounterPage.jsx
@@ -1,14 +1,30 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const { count, increment: handleClick } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
 
+  const increment = () => {
+    setCount((current) => current + 1);
+  };
+  const decrement = () => {
+    setCount((current) => current - 1);
+  };
   return (
-    <div>
-      <h1>Count is {count} </h1>
-      <Button onClick={handleClick}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count} </h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input type="number" className="p-1 m-3 bg-gray-50 border-gray-300" />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
