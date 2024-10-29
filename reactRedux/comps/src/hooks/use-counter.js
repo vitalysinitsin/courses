9a2472a71ba3e03e01846808f6cee1f66d278dd0
hungryvito <vsinitsin94@gmail.com>
2024-10-28 22:40:26 -0600

    implemented test custom hook

---
+++ reactRedux/comps/src/hooks/use-counter.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useCounter = (initialCount) => {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const increment = () => {
+    setCount((current) => current + 1);
+  };
+
+  return {
+    count,
+    increment,
+  };
+};
+
+export default useCounter;
