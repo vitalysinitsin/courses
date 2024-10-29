624ba5668b16eb54727492122bd3b233133f5d51
hungryvito <vsinitsin94@gmail.com>
2024-10-29 11:00:37 -0600

    finish the implementation of the counter form

--- reactRedux/comps/src/pages/CounterPage.jsx
+++ reactRedux/comps/src/pages/CounterPage.jsx
@@ -4,6 +4,7 @@ import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount((current) => current + 1);
@@ -11,6 +12,19 @@ function CounterPage({ initialCount }) {
   const decrement = () => {
     setCount((current) => current - 1);
   };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
+
   return (
     <Panel className="m-3">
       <h1 className="text-lg">Count is {count} </h1>
@@ -19,9 +33,14 @@ function CounterPage({ initialCount }) {
         <Button onClick={decrement}>Decrement</Button>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
-        <input type="number" className="p-1 m-3 bg-gray-50 border-gray-300" />
+        <input
+          type="number"
+          value={valueToAdd || ""}
+          onChange={handleChange}
+          className="p-1 m-3 bg-gray-50 border-gray-300"
+        />
         <Button>Add it!</Button>
       </form>
     </Panel>
