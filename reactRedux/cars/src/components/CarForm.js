69106b4df0ae53d8748a98f2887523c00c1c7c52
hungryvito <vsinitsin94@gmail.com>
2024-10-31 15:41:12 -0600

    implements carform component with car slice reducers

--- reactRedux/cars/src/components/CarForm.js
+++ reactRedux/cars/src/components/CarForm.js
@@ -1,10 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, changeCost, addCar } from "../store";
+
 function CarForm() {
-  const handleNameChange = (event) => {};
+  const { name, cost } = useSelector((state) => ({
+    name: state.form.name,
+    cost: state.form.cost,
+  }));
+  const dispatch = useDispatch();
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+  const handleCostChange = (event) => {
+    const carCost = parseInt(event.target.value) || 0;
+    dispatch(changeCost(carCost));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCar({ name, cost }));
+  };
 
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -14,6 +32,18 @@ function CarForm() {
               onChange={handleNameChange}
             />
           </div>
+          <div className="field">
+            <label className="label">Cost</label>
+            <input
+              className="input is-expanded"
+              value={cost || ""}
+              onChange={handleCostChange}
+              type="number"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
         </div>
       </form>
     </div>
