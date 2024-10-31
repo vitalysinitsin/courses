1313ba9d6b6db1103d8273204f5b38219099c528
hungryvito <vsinitsin94@gmail.com>
2024-10-31 13:11:44 -0600

    implements the movie slice with same funcitonality as song and utilizes it

--- reactRedux/drafts/src/store/index.js
+++ reactRedux/drafts/src/store/index.js
@@ -1,5 +1,19 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie: (state, action) => {
+      state.push(action.payload);
+    },
+    removeMovie: (state, action) => {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
 const songsSlice = createSlice({
   name: "song",
   initialState: [],
@@ -8,7 +22,8 @@ const songsSlice = createSlice({
       state.push(action.payload);
     },
     removeSong(state, action) {
-      //
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
     },
   },
 });
@@ -16,8 +31,10 @@ const songsSlice = createSlice({
 const store = configureStore({
   reducer: {
     songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
   },
 });
 
 export { store };
-export const { addSong } = songsSlice.actions;
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
