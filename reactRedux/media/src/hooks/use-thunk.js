31df01ddf88c2ab56987dac3e48c444ab3eebf70
hungryvito <vsinitsin94@gmail.com>
2024-11-04 14:43:54 -0700

    created reusable thunk hook

---
+++ reactRedux/media/src/hooks/use-thunk.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { useState, useCallback } from "react";
+
+export const useThunk = (thunk) => {
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const runThunk = useCallback(
+    (arg) => {
+      setIsLoading(true);
+      dispatch(thunk(arg))
+        .unwrap()
+        .catch((err) => setError(err))
+        .finally(() => setIsLoading(false));
+    },
+    [dispatch, thunk]
+  );
+
+  return [runThunk, isLoading, error];
+};
