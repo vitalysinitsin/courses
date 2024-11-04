b6295497bbce556c67fed20120f8b80aad80961f
hungryvito <vsinitsin94@gmail.com>
2024-11-04 16:04:12 -0700

    adds and wraps up the expandable panel + inits albums component

---
+++ reactRedux/media/src/components/ExpandablePanel.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
+function ExpandablePanel({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+  const handleClick = () => {
+    setExpanded((current) => !current);
+  };
+  return (
+    <div className="mb-2 border rounded" onClick={handleClick}>
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <div className="flex flex-row justify-between">{header}</div>
+        <div>{expanded ? <GoChevronDown /> : <GoChevronLeft />}</div>
+      </div>
+      {expanded && <div className="p-2 border-t">{children}</div>}
+    </div>
+  );
+}
+
+export default ExpandablePanel;
--- reactRedux/media/src/components/UserListItem.js
+++ reactRedux/media/src/components/UserListItem.js
@@ -2,6 +2,9 @@ import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import { Fragment } from "react";
+import AlbumsList from "./AlbumsList";
 
 function UserListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,18 +13,25 @@ function UserListItem({ user }) {
     doRemoveUser(user);
   };
 
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user.</div>}
-          {user.name}
+  const header = (
+    <Fragment>
+      <div className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          <div className="flex flex-row justify-between">
+            <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+              <GoTrashcan />
+            </Button>
+            {error && <div>Error deleting user.</div>}
+            {user.name}
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
+  );
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 }
 
