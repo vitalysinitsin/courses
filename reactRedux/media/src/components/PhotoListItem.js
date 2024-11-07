6c39e40be16f8c3e30a90256c4a4978b2e969142
hungryvito <vsinitsin94@gmail.com>
2024-11-06 23:11:42 -0700

    utilizes photo removal endpoint

--- reactRedux/media/src/components/PhotoListItem.js
+++ reactRedux/media/src/components/PhotoListItem.js
@@ -1,5 +1,20 @@
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 function PhotoListItem({ photo }) {
-  return <img className="h-20 w-20 m-2" src={photo.url} alt="album cover" />;
+  const [removePhoto] = useRemovePhotoMutation();
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+
+  return (
+    <div className="relative cursor-pointer m-2" onClick={handleRemovePhoto}>
+      <img className="h-20 w-20" src={photo.url} alt="album cover" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" />
+      </div>
+    </div>
+  );
 }
 
 export default PhotoListItem;
