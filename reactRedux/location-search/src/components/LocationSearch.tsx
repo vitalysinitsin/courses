15f23b57d3fb6bb904369082a103fae1fbf04e76
hungryvito <vsinitsin94@gmail.com>
2024-11-10 23:03:17 -0700

    short form submission with typescript

--- reactRedux/location-search/src/components/LocationSearch.tsx
+++ reactRedux/location-search/src/components/LocationSearch.tsx
@@ -6,7 +6,26 @@ interface LocationSearchProps {
 }
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [term, setTerm] = useState("");
   const [places, setPlaces] = useState<Place[]>([]);
 
-  return <div>Search for a location</div>;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
