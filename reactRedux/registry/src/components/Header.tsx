ef889b291e16c1e16a2bc380148f7eb74530d661
hungryvito <vsinitsin94@gmail.com>
2024-11-14 22:26:55 -0700

    add styling to the header and search input components

--- reactRedux/registry/src/components/Header.tsx
+++ reactRedux/registry/src/components/Header.tsx
@@ -3,9 +3,15 @@ import SearchInput from "./SearchInput";
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">NPM Registry</Link>
-      <SearchInput />
+    <div className="flex items-center justify-between px-4 border-b h-14">
+      <div className="flex items-center space-x-2 text-sm">
+        <Link to="/" className="text-lg font-bold">
+          NPM Registry
+        </Link>
+      </div>
+      <div className="w-full max-w-xl ml-4">
+        <SearchInput />
+      </div>
     </div>
   );
 }
--- reactRedux/registry/src/components/SearchInput.tsx
+++ reactRedux/registry/src/components/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { VscSearch } from "react-icons/vsc";
 
 export default function SearchInput() {
   const [term, setTerm] = useState("");
@@ -13,7 +14,17 @@ export default function SearchInput() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input value={term} onChange={(e) => setTerm(e.target.value)} />
+      <div className="relative">
+        <div className="absolute inset-y-0 flex items-center pl-3">
+          <VscSearch className="h-5 w-5 text-gray-500" />
+        </div>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="pl-10 py-2 w-full border-0 shadow-none"
+          placeholder="Search packages"
+        />
+      </div>
     </form>
   );
 }
