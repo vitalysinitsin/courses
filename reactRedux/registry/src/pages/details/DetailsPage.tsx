ef8bf98c4bd01cc1e60f3d60496a2ac6cce66b48
hungryvito <vsinitsin94@gmail.com>
2024-11-20 16:38:55 -0700

    renders the package details data

--- reactRedux/registry/src/pages/details/DetailsPage.tsx
+++ reactRedux/registry/src/pages/details/DetailsPage.tsx
@@ -4,8 +4,20 @@ import { useLoaderData } from "react-router-dom";
 function DetailsPage() {
   const { details } = useLoaderData() as DetailsLoaderResult;
   return (
-    <div>
-      {details.name} {details.description}
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold my-4">{details.name}</h1>
+      <div>
+        <h3 className="text-lg font-bold">Description</h3>
+        <div className="p3 bg-gray-200 rounded">{details.description}</div>
+      </div>
+      <div>
+        <h3 className="text-lg font-bold">Licesnse</h3>
+        <div className="p3 bg-gray-200 rounded">{details.license}</div>
+      </div>
+      <div>
+        <h3 className="text-lg font-bold">Author</h3>
+        <div className="p3 bg-gray-200 rounded">{details.author.name}</div>
+      </div>
     </div>
   );
 }
