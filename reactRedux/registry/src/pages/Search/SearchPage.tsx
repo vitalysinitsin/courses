c55c1ff15ad2053df334bca7b165210ab5a26c26
hungryvito <vsinitsin94@gmail.com>
2024-11-20 15:04:09 -0700

    rendered search results on the search page

--- reactRedux/registry/src/pages/Search/SearchPage.tsx
+++ reactRedux/registry/src/pages/Search/SearchPage.tsx
@@ -1,9 +1,18 @@
 import { useLoaderData } from "react-router-dom";
+import type { SearchLoaderResult } from "./searchLoader";
+import PackageListItem from "../../components/PackageListItem";
 
-function SearchPage() {
-  const data = useLoaderData();
+export default function SearchPage() {
+  const { searchResults } = useLoaderData() as SearchLoaderResult;
 
-  return <div>search</div>;
+  const renderedResults = searchResults.map((result) => (
+    <PackageListItem pack={result} key={result.name} />
+  ));
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold my-6">Search Page</h1>
+      <div className="space-y-4 mt-4">{renderedResults}</div>
+    </div>
+  );
 }
-
-export default SearchPage;
