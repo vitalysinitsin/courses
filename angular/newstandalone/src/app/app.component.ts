bae50879fb7d581d45ea2cb3f5e5fb2bf9fc8ca5
hungryvito <vsinitsin94@gmail.com>
2024-12-10 14:15:13 -0700

    pipe modules and data interpolation

--- angular/newstandalone/src/app/app.component.ts
+++ angular/newstandalone/src/app/app.component.ts
@@ -2,13 +2,37 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule, HomeComponent],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    HomeComponent,
+    CommonModule,
+    ReversePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'newstandalone';
+  title = 'Angular 19 re-freshner';
+  subtitle = 'general concepts';
+  todaysDate = new Date();
+  salary = 100000;
+  _obj = { name: 'NT' };
+  isDisabled = true;
+
+  ChangeTitle() {
+    this.title = 'Angular 19 updated title';
+  }
+
+  UpdateTitle(event: Event) {
+    if (event.currentTarget instanceof EventTarget) {
+      const input = event.currentTarget as HTMLInputElement;
+      this.title = input.value;
+    }
+  }
 }
