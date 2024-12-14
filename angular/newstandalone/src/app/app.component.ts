25e7bf3f16c1bc00161c7f66d64f32624cb0ff7f
hungryvito <vsinitsin94@gmail.com>
2024-12-13 19:13:39 -0700

    basic routing

--- angular/newstandalone/src/app/app.component.ts
+++ angular/newstandalone/src/app/app.component.ts
@@ -1,40 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './home/home.component';
-import { CommonModule } from '@angular/common';
-import { ReversePipe } from './custom/reverse.pipe';
-import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatCardModule,
-    HomeComponent,
-    CommonModule,
-    ReversePipe,
-    FormsModule,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIcon,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Angular 19 re-freshner';
-  subtitle = 'general concepts';
-  todaysDate = new Date();
-  salary = 100000;
-  _obj = { name: 'NT' };
-  isDisabled = true;
-
-  ChangeTitle() {
-    this.title = 'Angular 19 updated title';
-  }
-
-  UpdateTitle(event: Event) {
-    if (event.currentTarget instanceof EventTarget) {
-      const input = event.currentTarget as HTMLInputElement;
-      this.title = input.value;
-    }
-  }
-}
+export class AppComponent {}
--- angular/newstandalone/src/app/home/home.component.ts
+++ angular/newstandalone/src/app/home/home.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ReversePipe } from '../custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, CommonModule, ReversePipe, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  title = 'Angular 19 re-freshner';
+  subtitle = 'general concepts';
+  todaysDate = new Date();
+  salary = 100000;
+  _obj = { name: 'NT' };
+  isDisabled = true;
 
+  ChangeTitle() {
+    this.title = 'Angular 19 updated title';
+  }
+
+  UpdateTitle(event: Event) {
+    if (event.currentTarget instanceof EventTarget) {
+      const input = event.currentTarget as HTMLInputElement;
+      this.title = input.value;
+    }
+  }
 }
